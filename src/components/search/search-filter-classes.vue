7bf1e59628b0d42c33e0e3f67ebe71520ecd0a4a
Christopher Bebry <invalidsyntax@gmail.com>
2020-07-26 21:58:24 -0400

    Make search work again, and some cleanups.

--- src/components/search/search-filter-classes.vue
+++ src/components/search/search-filter-classes.vue
@@ -1,16 +1,14 @@
 <template>
     <div class="search-filter-classes">
         <b-form-group label="Class(s)">
-            <b-form-checkbox-group buttons v-model="selectedclass">
+            <b-form-checkbox-group buttons v-model="selectedClasses">
                 <b-form-checkbox
                     v-for="classOption in classOptions"
                     :key="classOption.value"
                     :value="classOption.value"
-                    @change.native="selectclass"
+                    @change.native="selectClass"
                 >
-                    <img
-                    :src="renderSymbol(classOption.symbol)"
-                    :alt="classOptions.text" />
+                    <img :src="renderSymbol(classOption.symbol)" :alt="classOptions.text" />
                 </b-form-checkbox>
             </b-form-checkbox-group>
         </b-form-group>
@@ -23,14 +21,18 @@ export default {
     props: ['value'],
     data() {
         return {
-            selectedclass: [],
+            selectedClasses: [],
             classOptions: [
                 { text: 'Knight', value: 'knight', symbol: 'symbol/icon_class_knight.png' },
                 { text: 'Warrior', value: 'warrior', symbol: 'symbol/icon_class_warrior.png' },
                 { text: 'Ranger', value: 'ranger', symbol: 'symbol/icon_class_ranger.png' },
                 { text: 'Thief', value: 'thief', symbol: 'symbol/icon_class_thief.png' },
                 { text: 'Mage', value: 'mage', symbol: 'symbol/icon_class_mage.png' },
-                { text: 'soulweaver', value: 'soulweaver', symbol: 'symbol/icon_class_soulweaver.png' }
+                {
+                    text: 'Soul Weaver',
+                    value: 'soulweaver',
+                    symbol: 'symbol/icon_class_soulweaver.png'
+                }
             ]
         }
     },
@@ -38,13 +40,13 @@ export default {
         renderSymbol(symbol) {
             return require(`@/assets/${symbol}`)
         },
-        selectclass() {
-            this.$emit('classSelection', this.selectedclasses)
+        selectClass() {
+            this.$emit('classSelection', this.selectedClasses)
         }
     },
     watch: {
         value() {
-            this.selectedclasses = this.value
+            this.selectedClasses = this.value
         }
     }
 }
@@ -52,16 +54,16 @@ export default {
 
 <style lang="less">
 .search-filter-classes label {
-  padding: 5px 8px;
-  user-select: none;
-  min-width: 18px;
-  text-align: center;
-  background: #2a313d;
-  color: #a1aebd;
-  border: 1px solid #222731;
-  border-radius: 3px;
-  box-shadow: none!important;
-  outline: 0;
+    padding: 5px 8px;
+    user-select: none;
+    min-width: 18px;
+    text-align: center;
+    background: #2a313d;
+    color: #a1aebd;
+    border: 1px solid #222731;
+    border-radius: 3px;
+    box-shadow: none !important;
+    outline: 0;
 }
 
 .search-filter-classes label.hover {
@@ -71,11 +73,9 @@ export default {
 .search-filter-classes label.active {
     background: #202225;
 }
-img{
-  height: 18px;
-  width: 18px;
-  vertical-align: sub;
+img {
+    height: 18px;
+    width: 18px;
+    vertical-align: sub;
 }
-
-
 </style>
